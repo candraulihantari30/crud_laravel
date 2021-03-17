3a4105a2ba2ec4b22341cee2771cccd1933454cc
unknown <kacandraulihantari30@gmail.com>
2021-03-17 20:41:58 +0800

    crudlaravel

---
+++ resources/views/admin/dasboard.blade.php
@@ -0,0 +1,12 @@
+<x-template-layout>
+    <h1 class="text-3xl text-black pb-6"> Data Dasboard</h1>
+
+    <div class="flex flex-wrap mt-6">
+       
+       
+    
+                </tbody>
+            </table>
+        </div>
+    </div>
+</x-template-layout>
